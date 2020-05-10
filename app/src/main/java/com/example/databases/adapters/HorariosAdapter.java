36479ea59c8872a4c9b109909c96c3b57544a0ed
jorge9797 <32504262+jorge9797@users.noreply.github.com>
2020-05-10 11:09:22 -0600

    Carga de horas y modificaciones en adaptador de horarios

--- app/src/main/java/com/example/databases/adapters/HorariosAdapter.java
+++ app/src/main/java/com/example/databases/adapters/HorariosAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,6 +37,16 @@ public class HorariosAdapter extends ArrayAdapter<Horario> {
         if(view==null){
             view = LayoutInflater.from(this.context).inflate(R.layout.item_horario , null);
         }
+
+        TextView disponibilidad = view.findViewById(R.id.tvDisponibilidad);
+        TextView hora = view.findViewById( R.id.tvHora   );
+        TextView fecha =  view.findViewById(R.id.tvFecha);
+
+        Horario  horario =  horarioList.get(position);
+
+        disponibilidad.setText( horario.getEstado()  );
+        hora.setText(  horario.getHoraInicio()  );
+//        fecha.setText(  horario.get  )
 
 
         return view;
--- app/src/main/java/com/example/databases/api/utilidades/Constantes.java
+++ app/src/main/java/com/example/databases/api/utilidades/Constantes.java
@@ -2,5 +2,5 @@ package com.example.databases.api.utilidades;
 
 public class Constantes {
 
-    public static final  String API_RESERVAS_CANCHAS_API  = "http://192.168.0.14/reservas-canchas-api/";
+    public static final  String API_RESERVAS_CANCHAS_API  = "http://192.168.0.16/reservas-canchas-api/";
 }
