f9d9a1e432c2327597dc6974f0b3721ac41c242d
jorge9797 <32504262+jorge9797@users.noreply.github.com>
2020-05-14 20:02:08 -0600

    Configuracion de red para dispositivos fisicos

--- app/src/main/java/com/example/databases/api/utilidades/Constantes.java
+++ app/src/main/java/com/example/databases/api/utilidades/Constantes.java
@@ -2,5 +2,6 @@ package com.example.databases.api.utilidades;
 
 public class Constantes {
 
-    public static final  String API_RESERVAS_CANCHAS_API  = "http://192.168.1.6:8080/reservas-canchas-api/";
+    public static final  String API_RESERVAS_CANCHAS_API  = "http://192.168.0.15/reservas-canchas-api/";
+
 }
